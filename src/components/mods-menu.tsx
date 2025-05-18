c0f312079db6ba9b9bc832b74e2b07d9242ae671
Sergio Baldomero Beltran Santiago <sergiobaldomero.beltransantiago@gmail.com>
2025-05-18 16:57:39 -0700

    Expandiendo la idea que tenia en mente, codigo anterior permanece comentado por si las dudas

--- src/components/mods-menu.tsx
+++ src/components/mods-menu.tsx
@@ -1,34 +1,41 @@
 "use client";
 
-import useModsFiles from "@/mod/hooks/use-mods-files";
+// import useModsFiles from "@/mod/hooks/use-mods-files";
+
+import { useEffect, useState } from "react";
+
+type ModCard = {
+  name: string;
+  icon: string;
+  version: string;
+  link: string;
+};
 
 export default function ModsMenu() {
-  const modsFiles = useModsFiles();
+  // const modsFiles = useModsFiles();
+
+  const [modFiles, setModFiles] = useState<ModCard[]>([]);
+
+  useEffect(() => {
+    fetch("/modFile.json")
+      .then((res) => res.json())
+      .then((data) => setModFiles(data));
+  }, []);
 
   return (
     <div className="p-4">
       <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-        {/*Ejemplo de lo que estoy buscando*/}
-        <li className="border-2 rounded-sm flex flex-col items-center justify-center text-gray-400 p-2 border-gray-400 overflow-clip">
-          <img
-            src="https://media.forgecdn.net/avatars/thumbnails/922/589/64/64/638388110702981994.png"
-            alt="mod icon"
-            className="w-16 h-16"
-          />
-          <span>Create: Resource Geodes</span>
-          <span>Version 1.20.1-0.1.0</span>
-          <a
-            href="https://www.curseforge.com/minecraft/mc-mods/create-resource-geodes/files/5272079"
-            target="_blank"
-            className="text-blue-500 underline"
-          >
-            Ir al mod
-          </a>
-        </li>
-        {/*Aqui se acaba el ejemplo*/}
-
-        {modsFiles.map((modFile) => (
+        {/*{modsFiles.map((modFile) => (
           <ModsMenuItem name={modFile.file} key={modFile.file} />
+        ))}*/}
+        {modFiles.map((mod) => (
+          <ModsMenuItemAlt
+            key={mod.name}
+            name={mod.name}
+            icon={mod.icon}
+            version={mod.version}
+            link={mod.link}
+          />
         ))}
       </ul>
     </div>
@@ -39,6 +46,19 @@ function ModsMenuItem({ name }: { name: string }) {
   return (
     <li className="border-2 rounded-sm flex items-center justify-center text-gray-400 p-2 border-gray-400 overflow-clip">
       {name}
+    </li>
+  );
+}
+
+function ModsMenuItemAlt({ name, icon, version, link }: ModCard) {
+  return (
+    <li className="border-2 rounded-sm flex flex-col items-center justify-center text-gray-400 p-2 border-gray-400 overflow-clip">
+      <img src={icon} alt="mod icon missing" className="w-16 h-16" />
+      <span>{name}</span>
+      <span>{version}</span>
+      <a href={link} target="_blank" className="text-blue-500 underline">
+        Ir al mod
+      </a>
     </li>
   );
 }
