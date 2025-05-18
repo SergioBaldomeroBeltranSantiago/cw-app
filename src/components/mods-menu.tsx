b59ade7503446d40047d6c3f36dd66cad5133eaf
Sergio Baldomero Beltran Santiago <sergiobaldomero.beltransantiago@gmail.com>
2025-05-18 14:39:36 -0700

    Idea de lo que tenia planeado en la pagina, esperando opiniones

--- src/components/mods-menu.tsx
+++ src/components/mods-menu.tsx
@@ -7,7 +7,26 @@ export default function ModsMenu() {
 
   return (
     <div className="p-4">
-      <ul className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+      <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+        {/*Ejemplo de lo que estoy buscando*/}
+        <li className="border-2 rounded-sm flex flex-col items-center justify-center text-gray-400 p-2 border-gray-400 overflow-clip">
+          <img
+            src="https://media.forgecdn.net/avatars/thumbnails/922/589/64/64/638388110702981994.png"
+            alt="mod icon"
+            className="w-16 h-16"
+          />
+          <span>Create: Resource Geodes</span>
+          <span>Version 1.20.1-0.1.0</span>
+          <a
+            href="https://www.curseforge.com/minecraft/mc-mods/create-resource-geodes/files/5272079"
+            target="_blank"
+            className="text-blue-500 underline"
+          >
+            Ir al mod
+          </a>
+        </li>
+        {/*Aqui se acaba el ejemplo*/}
+
         {modsFiles.map((modFile) => (
           <ModsMenuItem name={modFile.file} key={modFile.file} />
         ))}
