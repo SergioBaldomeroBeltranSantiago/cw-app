13dac8994333f42ec7ca03ee09788c7756056517
Brian Caldera <briancaldera07@gmail.com>
2025-05-18 23:32:17 -0400

    ✨  add search bar

--- src/components/CwSidebar.tsx
+++ src/components/CwSidebar.tsx
@@ -8,14 +8,18 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import SearchBar from "@/components/search-bar";
 
 export default function CwSidebar() {
   return (
-    <Sidebar>
+    <Sidebar className="border-gray-800">
       <SidebarContent className="bg-gray-900">
         <SidebarHeader className="text-gray-400">
           <h1 className="text-orange-600 text-3xl">CW APP .M.</h1>
         </SidebarHeader>
+        <SidebarGroup>
+          <SearchBar />
+        </SidebarGroup>
         <SidebarGroup>
           <SidebarGroupLabel className="text-gray-400 text-lg">
             Packs .m.
